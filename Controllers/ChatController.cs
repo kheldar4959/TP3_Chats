26a6432c1f58566721c43d12726197258671c20a
kheldar4959 <kevin.chartier2019@campus-eni.fr>
2020-07-22 14:31:25 +0200

    TP3

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP.Models;
+
+namespace TP3_Chats.Controllers
+{
+    public class ChatController : Controller
+    {
+        //Récupère liste à jour
+        private static List<Chat> chats = Chat.GetMeuteDeChats();
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(chats); ;
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            var chat = chats.FirstOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(chat);
+        }
+
+       
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var chat = chats.FirstOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var chat = chats.FirstOrDefault(c => c.Id == id);
+                if (chat != null)
+                {
+                    chats.Remove(chat);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
